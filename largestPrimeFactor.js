5080cca1c7db35e734f819df6b9b1a8a2596893c
ijerosimic <igor.jerosimic1@gmail.com>
2020-01-13 00:59:05 +0100

    Largest prime factor

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,19 +1,16 @@
-const largestPrimeFactor = num => {
+const largestPrimeFactor = (num) => {
     let maxFactor = 0;
     let limit = Math.sqrt(num);
+
+    if (isPrime(num))
+        return num;
 
     for (let i = 2; i < limit; i++) {
         if (isPrime(i) && (num % i === 0))
             maxFactor = i;
     }
 
-    return Array.from(Array(limit).keys())
-        .filter(x => isPrime(x) && (num % x === 0))
-        .map((x, y) => x - y);
-
-    return maxFactor === 0 ?
-        num :
-        maxFactor;
+    return maxFactor;
 }
 
 const isPrime = n => {
@@ -23,5 +20,6 @@ const isPrime = n => {
         if (n % i === 0)
             return false;
     }
+
     return true;
 }
